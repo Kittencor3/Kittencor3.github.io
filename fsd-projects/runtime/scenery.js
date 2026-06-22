bb95df99d30e0db482f70576d7306a63ffd9e876
Kittencor3 <taylor.robertson504@gmail.com>
2026-06-22 13:56:24 +0000

    changed project layout

--- fsd-projects/runtime/scenery.js
+++ fsd-projects/runtime/scenery.js
@@ -14,11 +14,15 @@ const scenery = {
   building: {
     imageUrl: "images/backgrounds/building.png",
     loopWidth: 1400,
-    instances: [{ x: 400, width: 100, height: 300, speedX: -2 }],
+    instances: [{ x: 0, width: 100, height: 300, speedX: -2 }],
+
+    imageUrl: "images/backgrounds/building.png",
+    loopWidth: 1400,
+    instances: [{ x: 0, width: 100, height: 300, speedX: -2 }],
   },
   lamp: {
     imageUrl: "images/backgrounds/lamp.png",
     loopWidth: 1400,
-    instances: [{ x: 700, width: 50, height: 150, speedX: 0 }],
+    instances: [{ x: 0, width: 50, height: 150, speedX: 0 }],
   },
 };
--- fsd-projects/runtime/student.js
+++ fsd-projects/runtime/student.js
@@ -1,5 +1,6 @@
 function moveScenery() {
   // TODO 2: Move background scenery based on current level speed
+  var buildingInstance = scenery.building.instances[i];
 }
 
 function generateLevel() {
